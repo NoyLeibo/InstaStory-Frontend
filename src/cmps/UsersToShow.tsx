b92c9dbd4e2527edc7e1546d132f8c0441591564
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-04-03 21:42:45 +0300

    test

--- src/cmps/UsersToShow.tsx
+++ src/cmps/UsersToShow.tsx
@@ -9,20 +9,23 @@ interface UsersToShowProps {
 export function UsersToShow(props: UsersToShowProps) {
     const navigate = useNavigate()
 
-    function navToUser(userId: string) {
+    function onNavToUser(userId: string) {
         navigate('/user/' + userId)
     }
     return (
         <section className="users-to-shown">
             {props.users.map((user, index) => (
                 <div className="flex row space-between margin8 divider20" key={index}>
-                    <div className="flex row justify-center align-center pointer" onClick={() => navToUser(user._id)}>
+                    <div className="flex row justify-center align-center pointer" onClick={() => onNavToUser(user._id)}>
                         <Avatar src={user.imgUrl || undefined} />
-                        <h4 className="margin0 marginleft8">{user.username}</h4>
+                        <div className="flex column">
+                            <h4 className="margin0 bold marginleft8">{user.username}</h4>
+                            <span className="fs14 margin0 marginleft8">{user.fullname}</span>
+                        </div>
                     </div>
-                    <div className="flex column align-center">
-                        <h5>{user.followers.length} followers</h5>
-                        <h5>{user.following.length} followers</h5>
+                    <div className="flex column align-center justify-center">
+                        <span className="fs14 bold margin0 marginleft8">{user.followers.length} followers</span>
+                        <span className="fs14 bold margin0 marginleft8">{user.following.length} followers</span>
                     </div>
                 </div >
             ))
